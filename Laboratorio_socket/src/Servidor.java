2c89643260231a7a9009cfe3be0d04bdbf13a6f9
marcos-ortega <mo205016@gmail.com>
2025-05-07 20:11:53 -0700

    Agregacion de puerto

--- Laboratorio_socket/src/Servidor.java
+++ Laboratorio_socket/src/Servidor.java
@@ -1,23 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Servidor {
-    
+
     public static void main(String[] args) throws IOException {
-       
 
-        String clientSentence;  
-        String capitalizedSentence; 
-        double base,altura,resultado;
-     ServerSocket welcomeSocket = new ServerSocket(6789);  
+        String clientSentence;
+        String capitalizedSentence;
+        double base, altura, resultado;
+        ServerSocket welcomeSocket = new ServerSocket(6789);
 
+        while (true) {
 
+            Socket connectionSocket = welcomeSocket.accept();
 
+            BufferedReader inFromClient
+                    = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
 
-
-
-
-
-
-}
+            DataOutputStream outToClient
+                    = new DataOutputStream(connectionSocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+    }
 }
