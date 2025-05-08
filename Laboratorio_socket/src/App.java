6a7043a9a2f6e84d695fd505a66f90e7ab4dc337
KristelSamantaDeFranco <kristel.defranco@gmail.com>
2025-05-07 20:23:35 -0600

    mi cambio

--- Laboratorio_socket/src/App.java
+++ Laboratorio_socket/src/App.java
@@ -3,21 +3,26 @@ import java.net.*;
 
 class App {
 
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
 
+                String sentence;
+                String modifiedSentence;
+                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        String sentence;
-        String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                // Conectarse al servidor en el puerto 6789
+                Socket clientSocket = new Socket("localhost", 6789); // Cambiar "localhost" por IP del servidor si es
+                                                                     // remoto
 
-    // Conectarse al servidor en el puerto 6789
-    Socket clientSocket = new Socket("localhost", 6789); // Cambiar "localhost" por IP del servidor si es remoto
+                // Flujos para enviar y recibir datos
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                // Pedir al usuario la base
+                System.out.print("Ingrese la base del rectángulo: ");
+                String base = inFromUser.readLine();
 
+                // Pedir al usuario la altura
+                System.out.print("Ingrese la altura del rectángulo: ");
+                String altura = inFromUser.readLine();
 
- // Flujos para enviar y recibir datos
- DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
- BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-
-    }
+        }
 }
