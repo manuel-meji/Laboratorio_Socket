d9ed6a4ff76de0e08fd591b21950ded1607b246a
KristelSamantaDeFranco <kristel.defranco@gmail.com>
2025-05-07 20:01:13 -0600

    modified:   Laboratorio_socket/src/App.java

--- Laboratorio_socket/src/App.java
+++ Laboratorio_socket/src/App.java
@@ -1,11 +1,6 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-<<<<<<< HEAD
-        System.out.println("Prueba telma");
-=======
-        System.out.println("Hello, World! 2");
-        Hola soy samanta
->>>>>>> 0a0dc50a52893f66a50aca53629d5966c36fd3e9
-    }
+        public static void main(String[] args) throws Exception {
+                System.out.println("Hello, World!");
+                System.out.println("Prueba telma");
+        }
 }
