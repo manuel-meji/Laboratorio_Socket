2e8acbd695ecda7cb5414ec63cf898af152dd62d
manuel-mejicano <manuelortizmejicano100@gmail.com>
2025-05-07 20:56:38 -0600

    solucion

--- Laboratorio_socket/src/App.java
+++ Laboratorio_socket/src/App.java
@@ -30,7 +30,7 @@ class TCPClient {
         outToServer.writeBytes(altura + "\n");
 
         // Esperar la respuesta del servidor
-        float respuesta = Float.parseFloat(inFromServer.readLine());
+        String respuesta = inFromServer.readLine();
         System.out.println("Área calculada por el servidor: " + respuesta);
 
         // Cerrar conexión
