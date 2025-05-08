e2407965845a45c167e45ecf5d4df65e4d6a3e48
David Avila Torres <dandida11@Hotmail.com>
2025-05-07 20:35:05 -0600

    cambios ID

--- Laboratorio_socket/src/App.java
+++ Laboratorio_socket/src/App.java
@@ -5,13 +5,14 @@ class App {
 
         public static void main(String[] args) throws Exception {
 
-                String sentence;
-                String modifiedSentence;
+                float base;
+                float altura;
+
                 BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
         
     // Conectarse al servidor en el puerto 6789
-    Socket clientSocket = new Socket("localhost", 6789); // Cambiar "localhost" por IP del servidor si es remoto
+    Socket clientSocket = new Socket("10.153.157.11", 6789); // Cambiar "localhost" por IP del servidor si es remoto
 
     // Flujos para enviar y recibir datos
     DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
