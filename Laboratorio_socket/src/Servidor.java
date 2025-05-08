a75c5904dd122d8c0c408a2fa367de07d1056af0
marcos-ortega <mo205016@gmail.com>
2025-05-07 20:07:31 -0700

    modified:   Laboratorio_socket/src/Servidor.java

--- Laboratorio_socket/src/Servidor.java
+++ Laboratorio_socket/src/Servidor.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Servidor {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
        
 
         String clientSentence;  
         String capitalizedSentence; 
         double base,altura,resultado;
-
+     ServerSocket welcomeSocket = new ServerSocket(6789);  
 
 
 
