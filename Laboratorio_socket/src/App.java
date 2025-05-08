20334046aa969ce18b116905d5c4aba2bb6d06e9
Josue Ortiz Bermudez <jortizbermudez14@gmail.com>
2025-05-07 20:06:35 -0600

    Cambio Josue2

--- Laboratorio_socket/src/App.java
+++ Laboratorio_socket/src/App.java
@@ -1,14 +1,22 @@
-
 import java.io.*;
 import java.net.*;
 
 class App {
 
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-                String sentence;
-                String modifiedSentence;
-                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
-        }
+        String sentence;
+        String modifiedSentence;
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+ // Conectarse al servidor en el puerto 6789
+ Socket clientSocket = new Socket("localhost", 6789); // Cambiar "localhost" por IP del servidor si es remoto
+
+ // Flujos para enviar y recibir datos
+ DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+ BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+    }
 }
